38712c7515cdd6cd0c11b9207f54ba4a668bd8e4
YuniAsyifa03 <yuniasyifa230@gmail.com>
2021-09-05 21:41:06 +0700

    Project Pertama

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pengenalan_suara_example/page/home_page.dart';
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Aplikasi Pengenal Suara ';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.cyan),
+        home: HomePage(),
+      );
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:clipboard/clipboard.dart';
+import 'package:flutter/material.dart';
+import 'package:pengenalan_suara_example/api/speech_api.dart';
+import 'package:pengenalan_suara_example/main.dart';
+import 'package:pengenalan_suara_example/widget/substring_highlighted.dart';
+import 'package:pengenalan_suara_example/utils.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String text = 'Tekan tombol dan mulai berbicara ! ';
+  bool isListening = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+          centerTitle: true,
+          actions: [
+            Builder(
+              builder: (context) => IconButton(
+                icon: Icon(Icons.content_copy),
+                onPressed: () async {
+                  await FlutterClipboard.copy(text);
+
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(content: Text('✓   Copied to Clipboard')),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          reverse: true,
+          padding: const EdgeInsets.all(30).copyWith(bottom: 150),
+          child: SubstringHighlight(
+            text: text,
+            terms: Command.all,
+            textStyle: TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+            textStyleHighlight: TextStyle(
+              fontSize: 32.0,
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+
+        
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: AvatarGlow(
+          animate: isListening,
+          endRadius: 150 ,
+          glowColor: Theme.of(context).primaryColor,
+          child: FloatingActionButton(
+            child: Icon(isListening ? Icons.mic : Icons.mic_none, size: 36),
+            onPressed: toggleRecording,
+          ),
+        ),
+      );
+
+  Future toggleRecording() => SpeechApi.toggleRecording(
+        onResult: (text) => setState(() => this.text = text),
+        onListening: (isListening) {
+          setState(() => this.isListening = isListening);
+
+          if (!isListening) {
+            Future.delayed(Duration(seconds: 1), () {
+              Utils.scanText(text);
+            });
+          }
+        },
+      );
+}
